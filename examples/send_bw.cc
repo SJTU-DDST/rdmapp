d9023d0e2a2f9f508e92eac0a91bcf080ec704a7
scarlet <phantom190514@gmail.com>
2025-11-10 19:47:03 +0800

    feat: send_bw with asio based

--- examples/send_bw.cc
+++ examples/send_bw.cc
@@ -1,14 +1,15 @@
-#include "acceptor.h"
-#include "connector.h"
-#include <algorithm>
+#include "asio/awaitable.hpp"
+#include "asio/co_spawn.hpp"
+#include "asio/detached.hpp"
+#include "listener.h"
+#include "qp_acceptor.h"
+#include "qp_connector.h"
 #include <cassert>
 #include <chrono>
-#include <exception>
 #include <iostream>
 #include <memory>
-#include <ratio>
+#include <spdlog/spdlog.h>
 #include <string>
-#include <thread>
 
 #include <rdmapp/rdmapp.h>
 
@@ -19,86 +20,84 @@ constexpr size_t kWorkerCount = 4;
 constexpr size_t kSendCount = 8 * 1024;
 constexpr size_t kPrintInterval = 1024;
 constexpr size_t kTotalSizeBytes = kBufferSizeBytes * kSendCount * kWorkerCount;
+constexpr size_t kGlobalThread = 8;
 
 template <bool Client = false>
-rdmapp::task<void> worker(size_t id, std::shared_ptr<rdmapp::qp> qp) {
+asio::awaitable<void> worker(size_t id, std::shared_ptr<rdmapp::qp> qp) {
   std::vector<uint8_t> buffer;
   buffer.resize(kBufferSizeBytes);
   auto local_mr = std::make_shared<rdmapp::local_mr>(
       qp->pd_ptr()->reg_mr(&buffer[0], buffer.size()));
-  std::cout << "Worker " << id << " started" << std::endl;
+  spdlog::info("worker: {} started", id);
   for (size_t i = 0; i < kSendCount; ++i) {
     if constexpr (Client) {
-      co_await qp->async_recv(local_mr);
+      co_await qp->recv(local_mr);
     } else {
-      co_await qp->async_send(local_mr);
+      co_await qp->send(local_mr);
     }
     if ((i + 1) % kPrintInterval == 0) {
-      std::cout << "Worker " << id << (Client ? " recv " : " sent ") << (i + 1)
-                << " times" << std::endl;
+      spdlog::info("worker {}: {} {} times", id, Client ? "recv" : "sent",
+                   i + 1);
     }
   }
-  std::cout << "Worker " << id << " exited" << std::endl;
+  spdlog::info("worker {} exited", id);
   co_return;
 }
 
 template <bool Client = false>
-rdmapp::task<void> handler(std::shared_ptr<rdmapp::qp> qp) {
+asio::awaitable<void> handler(std::shared_ptr<rdmapp::qp> qp) {
+  auto executor = co_await asio::this_coro::executor;
   std::vector<std::future<void> *> futures;
   for (size_t i = 0; i < kWorkerCount; ++i) {
-    auto task = worker<Client>(i, qp);
-    futures.emplace_back(&task.get_future());
-    task.detach();
+    asio::co_spawn(executor, worker<Client>(i, qp), asio::detached);
   }
-  auto tik = std::chrono::high_resolution_clock::now();
-  for (auto fut : futures) {
-    fut->get();
-  }
-  auto tok = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> seconds = tok - tik;
-  double mb = static_cast<double>(kTotalSizeBytes) / 1024 / 1024;
-  double throughput = mb / seconds.count();
-  std::cout << "Total: " << mb << " MB, Elapsed: " << seconds.count()
-            << " s, Throughput: " << throughput << " MB/s" << std::endl;
   co_return;
 }
 
-rdmapp::task<void> server(rdmapp::acceptor &acceptor) {
-  auto qp = co_await acceptor.accept();
-  co_await handler(qp);
-  co_return;
-}
-
-rdmapp::task<void> client(rdmapp::connector &connector) {
+asio::awaitable<void> client(rdmapp::qp_connector &connector) {
   auto qp = co_await connector.connect();
   co_await handler<true>(qp);
   co_return;
 }
 
 int main(int argc, char *argv[]) {
+  spdlog::set_level(spdlog::level::info);
   auto device = std::make_shared<rdmapp::device>(0, 1);
   auto pd = std::make_shared<rdmapp::pd>(device);
   auto cq = std::make_shared<rdmapp::cq>(device);
-  auto io_ctx = std::make_shared<asio::io_context>(4);
-  auto work_guard = asio::make_work_guard(*io_ctx);
-  std::jthread _([&io_ctx]() { io_ctx->run(); });
+  auto io_ctx = std::make_shared<asio::io_context>(
+      kGlobalThread); // NOTE:  thread for global io context
   auto executor = std::make_shared<rdmapp::executor>(io_ctx);
   auto cq_poller = std::make_shared<rdmapp::cq_poller>(cq, executor);
-  auto loop = rdmapp::socket::event_loop::new_loop();
-  auto looper = std::thread([loop]() { loop->loop(); });
   if (argc == 2) {
-    rdmapp::acceptor acceptor(loop, std::stoi(argv[1]), pd, cq);
-    server(acceptor);
+    auto work_guard = asio::make_work_guard(*io_ctx);
+    auto l = std::make_shared<rdmapp::listener>(std::stoi(argv[1]));
+    auto acc = std::make_shared<rdmapp::qp_acceptor>(pd, cq);
+    auto f = [acc](asio::ip::tcp::socket socket) -> asio::awaitable<void> {
+      auto qp = co_await acc->accept(std::move(socket));
+      co_await handler<false>(qp);
+    };
+    l->listen_and_serve(*io_ctx, std::move(f));
+    io_ctx->run();
   } else if (argc == 3) {
-    rdmapp::connector connector(loop, argv[1], std::stoi(argv[2]), pd, cq);
-    client(connector);
+    auto connector = rdmapp::qp_connector(argv[1], std::stoi(argv[2]), pd, cq);
+    asio::co_spawn(*io_ctx, client(connector), asio::detached);
+
+    auto tik = std::chrono::high_resolution_clock::now();
+    io_ctx->run();
+    auto tok = std::chrono::high_resolution_clock::now();
+    spdlog::info("client exit after communicated with {}:{}", argv[1], argv[2]);
+
+    std::chrono::duration<double> seconds = tok - tik;
+    double mb = static_cast<double>(kTotalSizeBytes) / 1024 / 1024;
+    double throughput = mb / seconds.count();
+    spdlog::info("total: {} MB, elapsed: {} s, throughput: {}", mb,
+                 seconds.count(), throughput);
+
   } else {
     std::cout << "Usage: " << argv[0] << " [port] for server and " << argv[0]
               << " [server_ip] [port] for client" << std::endl;
+    ::exit(-1);
   }
-  io_ctx->stop();
-  loop->close();
-  looper.join();
-  io_ctx->stop();
   return 0;
 }
