ec1bd7b90bc92903ced5e4b91f50a545c23d76f1
scarlet <phantom190514@gmail.com>
2025-11-23 15:21:44 +0800

    perf: avoid thread schedule

--- src/executor.cc
+++ src/executor.cc
@@ -9,17 +9,12 @@ executor::executor(std::shared_ptr<asio::io_context> io_context)
     : io_ctx_(std::move(io_context)) {}
 
 void executor::process_wc(struct ibv_wc const &wc) {
-  auto fn = [wc] {
-    spdlog::trace("process_wc: {:#x}", wc.wr_id);
-    auto cb = reinterpret_cast<callback_ptr>(wc.wr_id);
-    (*cb)(wc);
-    spdlog::trace("process_wc done: {:#x}", wc.wr_id);
-    destroy_callback(cb);
-    spdlog::trace("process_wc callback destroyed");
-  };
-
-  asio::post(*io_ctx_, fn);
-  spdlog::trace("submit process_wc task: {:#x}", wc.wr_id);
+  spdlog::trace("process_wc: {:#x}", wc.wr_id);
+  auto cb = reinterpret_cast<callback_ptr>(wc.wr_id);
+  (*cb)(wc);
+  spdlog::trace("process_wc done: {:#x}", wc.wr_id);
+  destroy_callback(cb);
+  spdlog::trace("process_wc callback destroyed");
 }
 
 void executor::shutdown() {}
